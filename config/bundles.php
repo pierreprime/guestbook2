543e5c3aadc4a84b318f32fac0087779954d240a
Pierre Prime <pierre.vermeil@gmail.com>
2020-05-15 23:26:33 +0200

    master added conference group and show routes

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,7 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,36 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/hello/{name}", name="conference")
+     * @Route("/conferences", name="homepage")
      */
-    public function index(string $name = '')
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository)
     {
-        $greet = '';
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
+    }
 
-        if($name){
-            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-        }
-
-        return new Response(<<<EOF
-<html>
-<body>
-$greet
-<img src="/images/wip.gif" />
-</body>
-</html>
-EOF
-);
-
-//        return $this->render('conference/index.html.twig', [
-//            'controller_name' => 'ConferenceController',
-//        ]);
+    /**
+     * @Route("/conference/{id}", name="conference")
+     */
+    public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository)
+    {
+        return new Response($twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']),
+        ]));
     }
 }
