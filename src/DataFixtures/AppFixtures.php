52bd34554c9428cddba3c427e297197ac92b6662
Pierre Vermeil <pierre.vermeil@gmail.com>
2020-05-28 23:56:03 +0200

    master unit and func test makefile

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Admin;
+use App\Entity\Comment;
+use App\Entity\Conference;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+
+class AppFixtures extends Fixture
+{
+    // we need this to re-create admin user at run
+    private $encoderFactory;
+
+    public function __construct(EncoderFactoryInterface $encoderFactory)
+    {
+        $this->encoderFactory = $encoderFactory;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $sydney = new Conference();
+        $sydney->setCity('Sydney');
+        $sydney->setYear('2000');
+        $sydney->setIsInternational(true);
+        $manager->persist($sydney);
+
+        $athens = new Conference();
+        $athens->setCity('Athens');
+        $athens->setYear('2004');
+        $athens->setIsInternational(true);
+        $manager->persist($athens);
+
+        $comment1 = new Comment();
+        $comment1->setConference($sydney);
+        $comment1->setAuthor('Myself');
+        $comment1->setEmail('myself@server.org');
+        $comment1->setText('This was a great game');
+        $manager->persist($comment1);
+
+        $admin = new Admin();
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setUsername('admin');
+        $admin->setPassword($this->encoderFactory->getEncoder(Admin::class)->encodePassword('admin', null));
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
---
+++ tests/Controller/ConferenceControllerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ConferenceControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/conferences');
+
+//        echo $client->getResponse();
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Give your feedback');
+    }
+
+    public function testCommentSubmission()
+    {
+        $client = static::createClient();
+        $client->request('GET', 'conference/athens-2004');
+        $client->submitForm('Submit', [
+            'comment_form[author]' => 'Myself',
+            'comment_form[text]' => 'This conference was antique',
+            'comment_form[email]' => 'me@automat.me',
+            'comment_form[photo]' => dirname(__DIR__, 2).'/public/images/wip.gif',
+        ]);
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorExists('div:contains("There are 1 comments")');
+    }
+
+    public function testConferencePage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/conferences');
+
+        $this->assertCount(2, $crawler->filter('h4'));
+        $client->clickLink('View');
+
+        $this->assertPageTitleContains('Sydney');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Sydney 2000');
+        $this->assertSelectorExists('div:contains("There are 1 comments")');
+    }
+}
